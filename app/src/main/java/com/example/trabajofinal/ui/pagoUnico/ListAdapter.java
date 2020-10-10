fdfd7a2877390b53dd7616b5960949b6a795f024
Nahuel-Scerca <56772334+Nahuel-Scerca@users.noreply.github.com>
2020-10-10 19:21:47 +0200

    comitea33

---
+++ app/src/main/java/com/example/trabajofinal/ui/pagoUnico/ListAdapter.java
@@ -0,0 +1,59 @@
+package com.example.trabajofinal.ui.pagoUnico;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.trabajofinal.R;
+import com.example.trabajofinal.modelo.Inmueble;
+import com.example.trabajofinal.modelo.Pago;
+
+import java.util.List;
+
+public class ListAdapter extends ArrayAdapter<Pago> {
+
+    private Context context;
+    private List<Pago> lista;
+    private LayoutInflater li;
+
+    public ListAdapter(@NonNull Context context, int resource, @NonNull List<Pago> objects , LayoutInflater li) {
+
+        super(context, resource, objects);
+        this.context = context;
+        this.lista= objects;
+        this.li=li;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View itemView= convertView;
+        if(itemView==null){
+            itemView=li.inflate(R.layout.item_pagos,parent,false);
+
+        }
+        Pago pago= lista.get(position);
+
+        TextView numContrato = itemView.findViewById(R.id.tvNumeroContrato);
+        numContrato.setText(pago.getNumero()+"");
+
+        TextView monto= itemView.findViewById(R.id.tvMonto);
+        monto.setText(pago.getContrato().getMontoAlquiler()+"");
+
+        TextView inquilino = itemView.findViewById(R.id.tvInquilino);
+        inquilino.setText(pago.getContrato().getInquilino().getNombre()+"");
+
+        TextView concepto = itemView.findViewById(R.id.tvConcepto);
+        concepto.setText(pago.getContrato().getInmueble().getDireccion()+"");
+
+        return itemView;
+
+    }
+}
